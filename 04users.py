cccfc76bc9335d00938550c01d6b9d78aaba9e04
Fernando Emanuel Gonzalez <44560431+fernandog22@users.noreply.github.com>
2022-04-26 17:16:36 -0300

    Update 04users.py

--- 04users.py
+++ 04users.py
@@ -1,7 +1,7 @@
 import json
 import urllib.request
 
-url = urllib.request.urlopen('https://gonzalezelectricista.com.ar/wp-json/wp/v2/users')
+url = urllib.request.urlopen('https://tusitiowp/wp-json/wp/v2/users')
 
 for i in json.loads(url.read()):
     user = i['slug']
